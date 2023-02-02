a7321994f84098fbe6a5458e9561f18ecced668d
geethika0601 <109510866+geethika0601@users.noreply.github.com>
2023-02-02 18:54:15 +0530

    Add files via upload

---
+++ DAY 1 PROGRAMS.py
@@ -0,0 +1,96 @@
+length=int(input("Enter length:"))
+breadth=int(input("Enter breadth:"))
+area=(length*breadth)
+print("area of rectangle:")
+print(area)
+
+
+n=float(input("Enter the number:"))
+multiply=3*n
+print(multiply)
+add=multiply+36.39
+print(add)
+final=add-30
+print(final)
+
+
+a,b=input("Enter").split(",")
+print(a)
+print(type(a))
+print(b)
+print(a+b)
+
+
+pnum=45
+nnum=-24.6
+multiply=(pnum*nnum)
+print(multiply)
+
+length=int(input("Enter length:"))
+breadth=int(input("Enter breadth:"))
+perimeter=2*(length+breadth)
+print("perimeter of rectangle:")
+print(perimeter)
+
+
+n=int(input("Enter integerno1:"))
+n1=int(input("Enter integerno2:"))
+n2=int(input("Enter integerno3:"))
+print("\n")
+n3=float(input("Enter floatno1:"))
+n4=float(input("Enter floatno2:"))
+n5=float(input("Enter floatno3:"))
+print("\n")
+n6=input("Enter string1:")
+n7=input("Enter string2:")
+print("\n")
+n8=complex(input("Enter complex1:"))
+print("OUTPUT")
+print("\n")
+print("First integer:",n)
+print("second integer:",n1)
+print("Third integer:",n2)
+print("\n")
+print("First float",n3)
+print("second float",n4)
+print("Third float",n5)
+print("\n")
+print("first string",n6)
+print("second string",n7)
+print("\n")
+print("first complex",n8)
+
+
+sc=int(input("Enter the amount of sugar candy kumar bought:"))
+print("kumar gave sam half of the sugar candy")
+sam=(sc/2)
+print(sam)
+print("sam returned to kumar half in what he got")
+kumar=(sam/2)
+print(kumar)
+final=(sam+kumar)
+print("The amount of candy kumar is having:")
+print(final)
+
+
+
+print("Before swap")
+a=int(input("Enter number1:"))
+b=int(input("Enter number2:"))
+temp=a
+a=b
+b=temp
+print("After swap")
+print(a)
+print(b)
+
+
+
+print("before swap:")
+a=int(input("Enter number1:"))
+b=int(input("Enter number2:"))
+b=b-a
+a=a+b
+print("After swap:")
+print(a)
+print(b)
